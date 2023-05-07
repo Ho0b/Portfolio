a31d41bd1268d004afbe93a52d9b834b4282ff08
Ho0b <netrahun47@gmail.com>
2023-05-07 17:40:58 -0400

    overhaul of portfolio website

--- README.md
+++
@@ -1,5 +0,0 @@
-# PortfolioWeb
-
-```
-my first website
-```
---
+++ script.js
@@ -0,0 +1,53 @@
+
+const gallerySection = document.getElementById("sect-gal");
+const workSection = document.getElementById("sect-work");
+const aboutSection = document.getElementById("sect-about");
+const skillsSection = document.getElementById("sect-skills");
+const contactSection = document.getElementById("sect-contact");
+
+const form = document.getElementById("form");
+
+function switchSectionGallery(){
+    gallerySection.classList.add("openedSection");
+    workSection.classList.remove("openedSection");
+    aboutSection.classList.remove("openedSection");
+    skillsSection.classList.remove("openedSection");
+    contactSection.classList.remove("openedSection");
+}
+function switchSectionWork(){
+    gallerySection.classList.remove("openedSection");
+    workSection.classList.add("openedSection");
+    aboutSection.classList.remove("openedSection");
+    skillsSection.classList.remove("openedSection");
+    contactSection.classList.remove("openedSection");
+}
+function switchSectionAbout(){
+    gallerySection.classList.remove("openedSection");
+    workSection.classList.remove("openedSection");
+    aboutSection.classList.add("openedSection");
+    skillsSection.classList.remove("openedSection");
+    contactSection.classList.remove("openedSection");
+}
+function switchSectionSkills(){
+    gallerySection.classList.remove("openedSection");
+    workSection.classList.remove("openedSection");
+    aboutSection.classList.remove("openedSection");
+    skillsSection.classList.add("openedSection");
+    contactSection.classList.remove("openedSection");
+}
+function switchSectionContact(){
+    gallerySection.classList.remove("openedSection");
+    workSection.classList.remove("openedSection");
+    aboutSection.classList.remove("openedSection");
+    skillsSection.classList.remove("openedSection");
+    contactSection.classList.add("openedSection");
+}
+
+window.onload = clearForms();
+
+function clearForms(){
+    document.querySelector("input#name").value = '';
+    document.querySelector("input#email").value = '';
+    document.querySelector("input#phone").value = '';
+    document.querySelector("input#message").value = '';
+}
